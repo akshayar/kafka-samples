25ed881915a952370c4b4809532c025f07c905b9
akshayar <akshaya.rawat@gmail.com>
2022-09-28 10:21:27 +0530

    Kafka Avro consumer with schema registry
    Kafka Avro consumer with schema registry

--- kafka-consumers/src/main/java/com/aksh/kafka/KafkaAvroConsumer.java
+++ kafka-consumers/src/main/java/com/aksh/kafka/KafkaAvroConsumer.java
@@ -4,23 +4,18 @@ import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.consumer.*;
 
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Properties;
 
 public class KafkaAvroConsumer {
-    private static String topicName = "data-stream-ingest-3";
+    private static String TOPIC_NAME = "data-stream-ingest-3";
 
     public static void main(String[] args) throws Exception{
-        Properties props = Common.loadConfig("src/main/resources/kafka.properties");
-
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group1");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroDeserializer");
-        props.put("schema.registry.url", "http://localhost:8081");
-
-
-        String topic = topicName;
+        Properties props = Common.loadConfig("src/main/resources/kafka-avro.properties");
+        String topicName= Optional.ofNullable(props.get("source.topic")).map(Objects::toString).orElse(TOPIC_NAME);
         final Consumer<String, GenericRecord> consumer = new KafkaConsumer<String, GenericRecord>(props);
-        consumer.subscribe(Arrays.asList(topic));
+        consumer.subscribe(Arrays.asList(topicName));
 
         try {
             while (true) {
